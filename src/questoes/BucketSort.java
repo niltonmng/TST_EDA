a08d77c4c8cd52fecc276b9bb69d21b2aa2e0da3
Nilton Ginani <Nilton Ginani>
2016-11-30 16:22:02 -0300

    radix, bucket e counting Sort

--- src/questoes/BucketSort.java
+++ src/questoes/BucketSort.java
@@ -4,18 +4,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BucketSort {
-	
-	
-	
-	public static void bucketSort(int[] vetor){
-		
-		List<Integer[]> baldes = new ArrayList<Integer[]>();
-		Integer[] balde = new Integer[10];
-		for (int i = 0; i < 10; i++) {
-			baldes.add(balde);
+
+	public static void main(String[] args) {
+
+		int[] v = new int[]{5,2,1,4,3};
+		for (int i = 0; i < v.length; i++) {
+			System.out.print(v[i] + " ");
 		}
-		
-		for (int i = 0; i < baldes.size(); i++) {
+		bucketSort(v);
+		System.out.println();
+		for (int i = 0; i < v.length; i++) {
+			System.out.print(v[i] + " ");
+		}
+		System.out.println();
+
+	}
+
+
+	public static void bucketSort(int[] a) {
+
+		int maior = a[0];
+		for (int i = 1; i < a.length; i++) {
+			if(a[i] > maior){
+				maior = a[i];
+			}
+		}
+		int [] bucket = new int[maior+1];
+
+		for (int i = 0; i < bucket.length; i++) {
+			bucket[i] = 0;
+		}
+
+		for (int i = 0; i < a.length; i++) {
+			bucket[a[i]]++;
+		}
+
+		int outPos = 0;
+		for (int i = 0; i < bucket.length; i++) {
+			for (int j = 0; j < bucket[i]; j++) {
+				a[outPos++]=i;
+			}
 		}
 	}
 
--- src/questoes/CountingSort.java
+++ src/questoes/CountingSort.java
@@ -2,28 +2,51 @@ package questoes;
 
 public class CountingSort {
 
-    public static int[] countingSort(int[] a, int k) {
-    
-        int[] c = new int[k];
 
-        // frequência
-        for (int i = 0; i < a.length; i++) {
-            c[a[i] - 1] += 1;
-        }
-        
-        // cumulativa
-        for (int i = 1; i < c.length; i++) {
-            c[i] += c[i-1];
-        }
+	public static void main(String[] args) {
 
-        int[] b = new int[a.length];
+		int[] v = new int[]{5,2,1,4,3};
+		for (int i = 0; i < v.length; i++) {
+			System.out.print(v[i] + " ");
+		}
+		System.out.println();
+		for (int i = 0; i < countingSort(v).length; i++) {
+			System.out.print(countingSort(v)[i] + " ");
+		}
+		System.out.println();
 
-        for (int i = a.length - 1; i >= 0; i--) {
-            b[c[a[i] - 1] -1] = a[i];
-            c[a[i] - 1] -= 1;
-        }
+	}
 
-        return b;
-    
-    }
+	public static int[] countingSort(int[] a) {
+
+		int maior = a[0];
+		for (int i = 1; i < a.length; i++) {
+			if(a[i]>maior){
+				maior = a[i];
+			}
+		}
+
+
+		int[] c = new int[maior];
+
+		// frequência
+		for (int i = 0; i < a.length; i++) {
+			c[a[i] - 1] += 1;
+		}
+
+		// cumulativa
+		for (int i = 1; i < c.length; i++) {
+			c[i] += c[i-1];
+		}
+
+		int[] b = new int[a.length];
+
+		for (int i = a.length - 1; i >= 0; i--) {
+			b[c[a[i] - 1] -1] = a[i];
+			c[a[i] - 1] -= 1;
+		}
+
+		return b;
+
+	}
 }
