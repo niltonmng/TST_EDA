fa2a27164351acbb0f2bfaab4453365a51d4e201
Nilton Ginani <Nilton Ginani>
2016-12-12 20:24:42 -0300

    queueteste

--- testes/QueueTest.java
+++ testes/QueueTest.java
@@ -32,5 +32,54 @@ public class QueueTest {
 		assertTrue(fila.add(29));
 		assertTrue(fila.isFull());
 	}
+	
+	
+	@Test
+	public void isEmpty_Full(){
+		Fila fila = new Fila(5);
+		assertTrue(fila.isEmpty());
+		fila.add(10);
+		assertFalse(fila.isEmpty());
+		assertFalse(fila.isFull());
+		fila.add(20);
+		fila.add(30);
+		fila.add(40);
+		fila.add(50);
+		assertFalse(fila.isEmpty());
+		assertTrue(fila.isFull());
+		
+		fila.remove();
+		fila.remove();
+		fila.remove();
+		fila.remove();
+		fila.remove();
+		
+		assertTrue(fila.isEmpty());
+		assertFalse(fila.isFull());
+	}
+	
+	
+	@Test
+	public void head(){
+		Fila fila = new Fila(5);
+		fila.add(10);
+		assertEquals(fila.head(),10);
+		fila.add(20);
+		assertEquals(fila.head(),10);
+		fila.add(30);
+		assertEquals(fila.head(),10);
+		fila.add(40);
+		assertEquals(fila.head(),10);
+		fila.add(50);
+		assertEquals(fila.head(),10);
+		fila.remove();
+		assertEquals(fila.head(),20);
+		fila.remove();
+		assertEquals(fila.head(),30);
+		fila.remove();
+		assertEquals(fila.head(),40);
+		fila.remove();
+		assertEquals(fila.head(),50);
+	}
 
 }
