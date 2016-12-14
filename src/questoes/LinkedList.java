e0105849056a091ad1ae55235888a1fb8fa76faa
Nilton Ginani <Nilton Ginani>
2016-12-14 11:54:10 -0300

    linkedList

---
+++ src/questoes/LinkedList.java
@@ -0,0 +1,120 @@
+package questoes;
+
+public class LinkedList {
+	
+	private Node head;
+	private int size;
+	
+	public LinkedList(){
+		this.head = null;
+		this.size = 0;
+	}
+	
+	public boolean isEmpty(){
+		return this.head == null;
+	}
+	
+	public void addLast(int n){
+		Node newNode = new Node(n);
+		
+		if(this.isEmpty()){
+			this.head = newNode;
+		}else{
+			Node aux = head;
+			while(aux.next != null){
+				aux = aux.next;
+			}
+			aux.next = newNode;
+		}
+		this.size += 1;
+	}
+	
+	private Node search(int n){
+		Node aux = head;
+		while(aux.next != null && aux.valor != n){
+			aux = aux.next;
+		}
+		return aux;
+	}
+	
+	public boolean remove(int n){
+		if(!isEmpty()){
+			
+			if(this.head.valor == n){
+				this.head = this.head.next;
+				this.size -= 1;
+				return true;
+			}
+			
+			Node prev = null;
+			Node aux = this.head;
+			
+			while(aux.next != null && aux.valor != n){
+				prev = aux;
+				aux = aux.next;
+			}
+			
+			if(aux == null){
+				return false;
+			}
+			else{
+				prev.next = aux.next;
+				size -= 1;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	public boolean removeLast(){
+		if(!isEmpty()){
+			if(this.head.next == null){
+				this.head = null;
+				this.size -= 1;
+				return true;
+			}
+			else{
+				Node aux = head;
+				Node prev = null;
+				
+				while(aux.next != null){
+					prev = aux;
+					aux = aux.next;
+				}
+				if(prev != null && aux == null){
+					prev.next = null;
+					this.size -= 1;
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public int size(){
+		return this.size;
+	}
+	
+	public String toString(){
+		String saida = "";
+		Node aux = head;
+		while(aux.next != null){
+			saida += aux.valor + " ";
+			aux = aux.next;
+		}
+		return saida.trim();
+	}
+
+}
+
+class Node {
+	
+	Node next;
+	int valor;
+	
+	public Node(int valor){
+		this.next = null;
+		this.valor = valor;
+	}
+}
